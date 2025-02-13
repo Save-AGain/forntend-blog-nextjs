6b24526a7e7f5606cce3f6f2cd0b4f918f14a0ef
Save-AGain <worawit.b6014841@gmail.com>
2025-02-13 17:41:24 +0700

    เพิ่ม domain
    เพิ่ม domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://yourwebsite.com",
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://saveagain.dev",
     generateRobotsTxt: true, // สร้าง robots.txt ด้วย
     sitemapSize: 5000, // จำนวน URL ต่อไฟล์ sitemap
   };
